ffd5d7b93131f53ef819ddae3f210fbc1366cc00
Kerem Karabulut <ke.kerem3@gmail.com>
2021-01-19 23:05:38 +0300

    Add project files.

---
+++ ClassMetotDemo1/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri Ekleniyor... \n" + customer.Id + " Nolu " + customer.CustomerName + " " + customer.CustomerSurname + "\nMüşteri Eklendi.");
+        }
+        public void Listing(params Customer[] customers)
+        {
+            Console.WriteLine("Müşteriler Listeleniyor... Lütfen Bekleyeniz.");
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Id + " Nolu " + customer.CustomerName + " " + customer.CustomerSurname + "\n" + "Toplam Varlık: " + customer.Asset + " TL" + "\n");
+            }
+            Console.WriteLine("Müşteriler Başarılı Bir Şekilde Listelendi.");
+        }
+        public void Deletion(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silme İşlemi Gerçekleştiriliyor... \n" + customer.Id + " Nolu " + customer.CustomerName + " " + customer.CustomerSurname + "\n" + "Müşteri Silme İşlemi Başarılı Bir Şekilde Tamamlandı.");
+        }
+    }
+}
---
+++ ClassMetotDemo1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 101;
+            customer1.CustomerName = "Doğan";
+            customer1.CustomerSurname = "ÖZYURT";
+            customer1.Asset = 255958.55;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 102;
+            customer2.CustomerName = "Enes";
+            customer2.CustomerSurname = "ÖZER";
+            customer2.Asset = 458373.79;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 103;
+            customer3.CustomerName = "Doğuş";
+            customer3.CustomerSurname = "ÖZYURT";
+            customer3.Asset = 101222.10;
+
+            Customer customer4 = new Customer();
+            customer4.Id = 104;
+            customer4.CustomerName = "Barış";
+            customer4.CustomerSurname = "KARABULUT";
+            customer4.Asset = 758292.21;
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4 };
+
+            CustomerManager musteriManager = new CustomerManager();
+            musteriManager.Add(customer1);
+            Console.WriteLine("\n ------------------- \n");
+
+            musteriManager.Listing(customers);
+            Console.WriteLine("\n ------------------- \n");
+
+            musteriManager.Deletion(customer1);
+        }
+    }
+}
